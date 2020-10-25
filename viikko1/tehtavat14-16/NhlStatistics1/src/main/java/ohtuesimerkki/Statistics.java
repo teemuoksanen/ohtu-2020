f08438b9d7bbea1498e2caa875f3b7cd6b3c2889
Teemu Oksanen <github@teemuoksanen.fi>
2020-10-25 16:43:06 +0200

    Korjattu topScorers-metodi palauttamaan annettu määrä

--- viikko1/tehtavat14-16/NhlStatistics1/src/main/java/ohtuesimerkki/Statistics.java
+++ viikko1/tehtavat14-16/NhlStatistics1/src/main/java/ohtuesimerkki/Statistics.java
@@ -42,7 +42,7 @@ public class Statistics {
         ArrayList<Player> topScorers = new ArrayList<Player>();
         Iterator<Player> playerIterator = players.iterator();
         
-        while (howMany>=0) {
+        while (howMany>0) {
             topScorers.add( playerIterator.next() );            
             howMany--;
         }
