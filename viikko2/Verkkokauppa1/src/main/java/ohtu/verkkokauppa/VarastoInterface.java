d0869528bdb50b37b66b60443ad8aa76b3720b78
Teemu Oksanen <github@teemuoksanen.fi>
2020-11-07 17:38:51 +0200

    Tehtävä 2.9 tehty

---
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/VarastoInterface.java
@@ -0,0 +1,13 @@
+package ohtu.verkkokauppa;
+
+public interface VarastoInterface {
+
+    Tuote haeTuote(int id);
+
+    int saldo(int id);
+
+    void otaVarastosta(Tuote t);
+
+    void palautaVarastoon(Tuote t);
+    
+}
