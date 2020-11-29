eaad4f4311df36752bebe04ebde42fc3053b5c5a
Teemu Oksanen <github@teemuoksanen.fi>
2020-11-29 20:09:48 +0200

    Tehtävä 6.5 tehty

---
+++ viikko6/QueryLanguage/src/main/java/statistics/QueryBuilder.java
@@ -0,0 +1,32 @@
+package statistics;
+
+import statistics.matcher.*;
+
+public class QueryBuilder {
+
+  Matcher matcher;
+
+  public QueryBuilder() {
+    this.matcher = new All();
+  }
+
+  public QueryBuilder hasAtLeast(int value, String category) {
+    this.matcher = new And(new HasAtLeast(value, category), matcher);
+    return this;
+  }
+
+  public QueryBuilder hasFewerThan(int value, String category) {
+    this.matcher = new And(new HasFewerThan(value, category), matcher);
+    return this;
+  }
+
+  public QueryBuilder playsIn(String team) {
+    this.matcher = new And(new PlaysIn(team), matcher);
+    return this;
+  }
+
+  public Matcher build() {
+    return matcher;
+  }
+  
+}
